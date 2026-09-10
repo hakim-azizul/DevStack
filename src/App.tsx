060574378e84fcc605cac4bcebc960c056c78cc7
kordomakto <hakimsanny898@gmail.com>
2026-09-10 23:50:54 +0300

    json data fetched and displayed in the Techs component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import { ToastContainer } from 'react-toastify'
 import './App.css'
 import Nav from './Components/Nav'
 import Banner from './Components/Banner'
+import { useState } from 'react'
+import type { Itech } from './types'
 
+const fetchTech = async ():Promise<Itech> =>{
+  const res = await fetch('./data.json');
+  const data = await res.json();
+  return data;
+}
 function App() {
+  const [techPromise] = useState(()=> fetchTech())
+
   return (
     <div >
       <Nav />
