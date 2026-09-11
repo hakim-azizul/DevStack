17ffa9d730a0bbee77da6ea2b1ada534f57bdd0d
kordomakto <hakimsanny898@gmail.com>
2026-09-11 06:30:08 +0300

    Created TechCard component. and sent tech data to TechCard for rendering technology items.

---
+++ src/Components/Technologies/TechCard.tsx
@@ -0,0 +1,12 @@
+import type { Itech } from "../../types";
+
+export interface TechCardProps {
+    tech: Itech
+}
+
+const TechCard = ({ tech }: TechCardProps) => {
+    
+    return 
+}
+
+export default TechCard;
--- src/Components/Technologies/Techs.tsx
+++ src/Components/Technologies/Techs.tsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import type { Itech } from "../../types";
+import TechCard from "./TechCard";
 
 export interface TechsProps {
     techPromise: Promise<Itech[]>
@@ -11,15 +12,9 @@ const Techs = ({techPromise}: TechsProps) => {
     return (
         <>
         {
-            techs.map(tech =>{
-                return (
-                    <div>
-                        <img src={tech.icon} alt={tech.name} className="w-16" />
-                        <h1>{tech.name}</h1>
-                    </div>
-
-                )
-            })
+            <div className="lg:col-span-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {technologies.map(tech:Itech => <TechCard key={tech.id} tech={tech} />)}
+            </div>
         }
         </>
     )
